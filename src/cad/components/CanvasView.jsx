f93d9da4c23a0420040f4b07dd2c9c11df6c16bf
W0NDRYS <jirka.wondrak@gmail.com>
2026-03-29 15:10:26 +0200

    Update CanvasView.jsx

--- src/cad/components/CanvasView.jsx
+++ src/cad/components/CanvasView.jsx
@@ -5,20 +5,39 @@ import { useCad } from "../context/CadContext";
 import DrawingSvg from "./DrawingSvg";
 
 export default function CanvasView() {
-  const { svgRef, handlePointerDown, handlePointerMove, handlePointerUp, handleDoubleClick } = useCad();
+  const {
+    svgRef,
+    handlePointerDown,
+    handlePointerMove,
+    handlePointerUp,
+    handleDoubleClick,
+  } = useCad();
 
   return (
     <div style={styles.canvasArea}>
       <div style={styles.card}>
         <div style={styles.cardHeader}>
           <h2 style={styles.title}>Výkres</h2>
-          <div style={styles.toolbarMeta}><Grid3X3 size={16} /><span>{SVG_W} × {SVG_H} mm</span></div>
+          <div style={styles.toolbarMeta}>
+            <Grid3X3 size={16} />
+            <span>
+              {SVG_W} × {SVG_H} mm
+            </span>
+          </div>
         </div>
+
         <div style={styles.canvasWrap}>
           <svg
             ref={svgRef}
             viewBox={`0 0 ${SVG_W} ${SVG_H}`}
-            style={{ width: "100%", height: "100%", display: "block", touchAction: "none", userSelect: "none" }}
+            preserveAspectRatio="xMidYMid meet"
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "block",
+              touchAction: "none",
+              userSelect: "none",
+            }}
             onPointerDown={handlePointerDown}
             onPointerMove={handlePointerMove}
             onPointerUp={handlePointerUp}
